03f0415de4ee0c26a63b96123afd1b0dc9337616
Jed Lynch <jedlynch@Jeds-MacBook-Pro.local>
2025-09-26 16:56:17 -0400

    tracker component

--- src/Tracker.jsx
+++ src/Tracker.jsx
@@ -15,6 +15,10 @@ export default function Tracker() {
         setAttempts(() => attempts + 1);
     }
 
+    let failed = useCallback(() => {
+        return attempts - success;
+    }, [success, attempts]);
+
     let percentage = useCallback(() => {
         return attempts === 0 ? 0 : (success/attempts * 100).toFixed(2);
     }, [success, attempts]);
@@ -22,33 +26,37 @@ export default function Tracker() {
 
     return (
         <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-2xl shadow-lg">
-            <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
-            Scoring App
+            <h1 className="text-3xl font-bold text-center text-gray-600 mb-6">
+            Scoring Tracker
             </h1>
 
-        <div className="grid grid-cols-2 gap-4 text-center mb-8">
-            <div className="p-4 bg-gray-50 rounded-lg shadow-sm">
-                <span className="block text-sm text-gray-500">Attempts</span>
-                <span className="text-2xl font-semibold text-gray-800">{attempts}</span>
-            </div>
+            <div className="grid grid-cols-2 gap-4 text-center mb-8">
+                <div className="col-span-2 p-4 bg-gray-50 rounded-lg shadow-sm">
+                    <span className="block text-sm text-gray-500">Attempts</span>
+                    <span className="text-2xl font-semibold text-gray-600">{attempts}</span>
+                </div>
                 <div className="p-4 bg-gray-50 rounded-lg shadow-sm">
                     <span className="block text-sm text-gray-500">Success</span>
                     <span className="text-2xl font-semibold text-green-600">{success}</span>
                 </div>
                 <div className="p-4 bg-gray-50 rounded-lg shadow-sm">
+                    <span className="block text-sm text-gray-500">Failed</span>
+                    <span className="text-2xl font-semibold text-red-600">{failed()}</span>
+                </div>
+                <div className="col-span-2 p-4 bg-gray-50 rounded-lg shadow-sm">
                     <span className="block text-sm text-gray-500">Percentage</span>
                     <span className="text-2xl font-semibold text-blue-600">
                     {percentage()}%
                     </span>
                 </div>
-                <div className="p-4 bg-gray-50 rounded-lg shadow-sm">
-                    <span className="block text-sm text-gray-500">Total</span>
-                    <span className="text-2xl font-semibold text-purple-600">{total}</span>
+                <div className="col-span-2 p-4 bg-gray-50 rounded-lg shadow-sm">
+                    <span className="block text-sm text-gray-500">Total Points</span>
+                    <span className="text-2xl font-semibold text-gray-600">{total}</span>
                 </div>
             </div>
 
             <div className="mb-6">
-                <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">Attempts Tracker</h2>
+                <h3 className="text-2xl font-bold text-center text-gray-600 mb-6">Attempts</h3>
             </div>
             <div className="flex flex-col sm:flex-row gap-4">
                 <div
